85cbd712ae9057bd55badd60633cc87c1e6affee
Carolina Campos <carolinacampos.dev@gmail.com>
2025-05-04 16:55:52 -0300

    test: adicionar teste para botão com ID dinâmico

--- cypress/e2e/ui-test.cy.js
+++ cypress/e2e/ui-test.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+describe('UI Test', () => {
+  it('Deve clicar no botão sem usar o ID dinâmico', () => {
+    cy.visit('/dynamicid')
+
+    cy.contains('button', 'Button with Dynamic ID')
+      .should('be.visible')
+      .click()
+      .should('have.text', 'Button with Dynamic ID')
   })
+
 })
