45ed577172624c765380a66cb42d5e76f3405f22
Carolina Campos <carolinacampos.dev@gmail.com>
2025-05-04 19:42:14 -0300

    test: garante que botão verde não pode ser clicado duas vezes
    O teste deve verificar que o botão verde não pode ser clicado duas vezes, pois após o primeiro clique, ele é sobreposto

--- cypress/e2e/ui-test.cy.js
+++ cypress/e2e/ui-test.cy.js
@@ -8,13 +8,26 @@ describe('UI Test', () => {
       .should('have.text', 'Button with Dynamic ID')
   })
 
-  it.only('Deve exibir alerta ao clicar no botão primário', () => {
+  it('Deve exibir alerta ao clicar no botão primário', () => {
     cy.visit('/classattr')
 
     cy.on('window:alert', cy.stub().as('alert'))
     cy.get('.btn-primary').click()
     cy.get('@alert')
       .should('have.been.calledWith', 'Primary button pressed')
+  })
+
+  it.only('Deve impedir clique duplo no botão verde', () => {
+    cy.visit('/hiddenlayers')
+
+    cy.get('#greenButton').click()
+    cy.get('#greenButton').click({ force: true })
+
+    
+    cy.get('#blueButton')
+      .should('exist')
+      .and('have.length', 1)
+
   })
 
 })
